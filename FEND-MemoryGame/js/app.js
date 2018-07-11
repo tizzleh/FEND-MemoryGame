257c8d6de0281e6cb8c8a475bf0d3c08208b7100
Ty Harlacker <harlacker@gmail.com>
2018-07-11 00:42:57 -0600

    feat: Added logic for matching cards and umatched cards.
    Refactored app.js

--- FEND-MemoryGame/js/app.js
+++ FEND-MemoryGame/js/app.js
@@ -5,13 +5,13 @@
 
 // let card = document.querySelectorAll('li.card');
 const DECK_EL = $('ul.deck'); // Deck element.
-const CARD_EL = $('.card'); // Deck element.
-const CARD = $('card').each(function(index) {
-  console.log(index + ": " + $(this).text());
-});
-CARD.on('click', function() {
-  alert('hello');
-});
+// const CARD_EL = $('.card'); // Deck element.
+// const CARD = $('card').each(function(index) {
+//   console.log(index + ": " + $(this).text());
+// });
+// CARD.on('click', function() {
+//   alert('hello');
+// });
 
 const ALL_CARDS = ['fa-leaf',
   'fa-diamond',
@@ -25,6 +25,7 @@ const ALL_CARDS = ['fa-leaf',
 
 // Set variables, use according to style-guide.
 let moves = 0;
+let flippedCards = [];
 let currentTime = 0;
 let numSolved = 0;
 let begin = false;
@@ -44,58 +45,73 @@ function shuffle(arr) {
     .sort((a, b) => a[0] - b[0])
     .map((a) => a[1]);
 }
-// Event handler should be called after creation of card elements.
-function cardClicked(e) {
 
-}
-// card.click();
-
-// card.on('click', function() {
-//   alert('clicked');
-// });
-// init game
 function initialize() {
-  // let card = document.querySelector('.card');
-  // let allCards = card.querySelectorAll();
-  // card.onclick = showAlert;
-  // CARD_EL.on('click', function() {
-  // cards array holds all cards
-  let card = document.getElementsByClassName('card');
-  let cards = [...card];
+  card = document.getElementsByClassName('card');
+  let cards = [...card]; // We don't know length. ES6 allows this.
   // loop to add event listeners to each card
-  for (var i = 0; i < cards.length; i++) {
+  for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener('click', cardClick);
   };
   // cardClicked();
   // });
 }
+window.onload = function() {
+  populateCards();
+  initialize();
+};
 
-function cardClick() {
-  this.classList.toggle("open");
-  this.classList.toggle("show");
-  this.classList.toggle("disabled");
+
+function incorrect() {
+  console.log('incorrect');
+  flippedCards[0].classList.remove('show', 'open', 'match');
+  flippedCards[1].classList.remove('show', 'open', 'match');
+  flippedCards = [];
 }
 
-function showAlert(event) {
-  alert('clicked');
+
+function correct() {
+  console.log('correct');
+  console.log(flippedCards[0]);
+  flippedCards[0].classList.add('show', 'open', 'match', 'animated', 'pulse');
+  flippedCards[1].classList.add('show', 'open', 'match', 'animated', 'pulse');
+  flippedCards = [];
 }
-$('.card').on('click', function() {
-  // showAlert();
-  alert('hello');
-  // clickCard();
-  // return $(this).firstChild.className
-});
+
+function cardClick(event) {
+  let selectedCardClass = this.firstChild.className;
+  let length = flippedCards.length;
+  flippedCards.push(this);
+  this.classList.toggle('open');
+  this.classList.toggle('show');
+  if (length == 1) {
+    console.log('length = 2');
+    console.log(flippedCards[0].firstChild.className);
+    if (flippedCards[0].firstChild.className == flippedCards[1].firstChild.className) {
+      console.log('matchedSets');
+      if (flippedCards[0].id === flippedCards[1].id) {
+
+        correct();
+      }
+    } else {
+      setTimeout(function() { // Card flips too quickly too see without timeout.
+        incorrect();
+      }, 400);
+    }
+  }
+}
+
 
 
 function cardClass(cardClass) {
   return firstChild.className;
 }
 
+
 // create individual card element
 function createCard(cardClass) {
   DECK_EL.append(`<li class="card"><i class="fa ${cardClass}"></i></li>`);
 }
-
 // Add random classes to each card element.
 function populateCards() {
   shuffle(ALL_CARDS // Shuffle cards
@@ -114,13 +130,3 @@ function clickCard(e) {
   let cardClass = $(this).attr('class');
   console.log(classes);
 }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
