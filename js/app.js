8400c4241a66be0c9df196e0f327a2364933579e
Ty Harlacker <harlacker@gmail.com>
2018-07-12 12:24:23 -0600

    feat: Fixed folder structure

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 
 // let card = document.querySelectorAll('li.card');
 const DECK_EL = $('ul.deck'); // Deck element.
-// const CARD_EL = $('.card'); // Deck element.
+// const CARD_EL = $('.card'); // Card element.
 // const CARD = $('card').each(function(index) {
 //   console.log(index + ": " + $(this).text());
 // });
@@ -29,7 +29,6 @@ let flippedCards = [];
 let currentTime = 0;
 let numSolved = 0;
 let begin = false;
-let visibleCards = []; // An array is used to compare visible cards.
 let timer;
 let gameOn = false;
 /*
@@ -49,23 +48,46 @@ function shuffle(arr) {
 function initialize() {
   card = document.getElementsByClassName('card');
   let cards = [...card]; // We don't know length. ES6 allows this.
-  // loop to add event listeners to each card
+  // Iterate over cards and add event listener.
   for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener('click', cardClick);
   };
-  // cardClicked();
-  // });
 }
 window.onload = function() {
   populateCards();
   initialize();
 };
 
+function removeShakeClass() {
+  // flippedCards[0].classList.remove('shake');
+  // flippedCards[1].classList.remove('shake');
+  $('.shake').removeClass('shake');
+}
 
 function incorrect() {
   console.log('incorrect');
+  // if (flippedCards[0].classList.contains('card') {
+  //     // console.log('Contains Class "card"');
+  //   });
+  // if (flippedCards[0].classList.contains('shake') || flippedCards[1].classList.contains('shake')) {
+  //   console.log('Contains shake');
+  //   removeShakeClass();
+  //
+  // }
+  // setTimeout(function() {
+
+  // flippedCards[0].classList.toggle('shake', 'animated');
+  // flippedCards[1].classList.add('shake', 'animated');
+  // }, 400);
+  $('.shake').removeClass('shake');
   flippedCards[0].classList.remove('show', 'open', 'match');
+  // flippedCards[0].classList.remove('animated', 'shake');
+  flippedCards[0].classList.add('animated', 'shake');
+  flippedCards[1].classList.add('animated', 'shake');
   flippedCards[1].classList.remove('show', 'open', 'match');
+  setTimeout(function() {
+    removeShakeClass();
+  }, 400);
   flippedCards = [];
 }
 
@@ -76,6 +98,17 @@ function correct() {
   flippedCards[0].classList.add('show', 'open', 'match', 'animated', 'pulse');
   flippedCards[1].classList.add('show', 'open', 'match', 'animated', 'pulse');
   flippedCards = [];
+  numSolved++;
+  console.log(numSolved);
+  isGameWon();
+}
+
+function isGameWon() {
+
+  if (numSolved == 8) {
+    console.log('You won');
+    alert('you wong')
+  }
 }
 
 function cardClick(event) {
@@ -84,30 +117,35 @@ function cardClick(event) {
   flippedCards.push(this);
   this.classList.toggle('open');
   this.classList.toggle('show');
+  // this.removeEventListener('click', cardClick); // Crude way to keep user from selecting twice
   if (length == 1) {
     console.log('length = 2');
     console.log(flippedCards[0].firstChild.className);
-    if (flippedCards[0].firstChild.className == flippedCards[1].firstChild.className) {
+    if (flippedCards[0].firstChild.className === flippedCards[1].firstChild.className) {
       console.log('matchedSets');
       if (flippedCards[0].id === flippedCards[1].id) {
-
+        removeShakeClass();
         correct();
       }
     } else {
-      setTimeout(function() { // Card flips too quickly too see without timeout.
+      setTimeout(function() { // Card flips too quickly to see without timeout.
         incorrect();
       }, 400);
     }
   }
 }
-
-
-
-function cardClass(cardClass) {
-  return firstChild.className;
+// initialize stars display
+function initStars() {
+  for (let i = 0; i < 3; i++) {
+    $(".stars").append(`<li><i class="fa fa-star"></i></li>`);
+  }
 }
 
-
+// reduce star rating
+function decStar() {
+  let stars = $(".fa-star");
+  $(stars[stars.length - 1]).toggleClass("fa-star fa-star-o");
+}
 // create individual card element
 function createCard(cardClass) {
   DECK_EL.append(`<li class="card"><i class="fa ${cardClass}"></i></li>`);
@@ -119,14 +157,22 @@ function populateCards() {
     .forEach(createCard); // Iterate through cards and append.
 }
 
-function startGame() {
-  DECK_EL.on('click', function() {
-
-    alert('hello');
-  });
+// starts the timer
+function startTimer() {
+  currentTime += 1;
+  $("#timer").html(currentTime);
+  seconds = setTimeout(startTimer, 1000);
+  if (seconds > 60) {
+    min = seconds / 60;
+    console.log(min);
+  }
 }
-// Check if game is running on click event, start if needed. Check visible cards.
-function clickCard(e) {
-  let cardClass = $(this).attr('class');
-  console.log(classes);
+
+// increment move count
+function incrementMove() {
+  moves += 1;
+  $("#moves").html(moves);
+  if (moves === 14 || moves === 20) {
+    decStar();
+  }
 }
